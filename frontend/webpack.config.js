0f8d25ccbcde7ce15d247f3d926088392c6f1aa0
bellacorcillo <isabellacorcillo@gmail.com>
2023-09-04 18:58:05 -0700

    first commit

---
+++ frontend/webpack.config.js
@@ -0,0 +1,32 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: './src/index.jsx',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],  // added this
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+  devServer: {
+    port: 3000,
+    open: true,
+  },
+};
